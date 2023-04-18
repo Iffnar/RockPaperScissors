d2b7d3eab845f368af2a2bfd74b39bc0e04be33b
Stephan Kleefisch <iffnar@Xubuntu.myguest.virtualbox.org>
2023-04-18 23:00:17 +0200

    write pseudocode and basic functionality

--- javascript.js
+++ javascript.js
@@ -1,1 +1,36 @@
-console.log(1)
+//get choice-input from player (Rock, paper or scissor)
+let playerChoice = (prompt("Choose your weapon: Rock, Paper or Scissors"))
+console.log("Player chooses "+ playerChoice)
+
+//get random number 
+const randomNumber = ((Math.floor((Math.random()*3)+1)));
+
+// convert number to computerChoice
+function assignNumber() {
+    if(randomNumber === 1) {
+        computerChoice = "Rock";
+    } else if (randomNumber === 2) {
+        computerChoice = "Scissors";
+    } else computerChoice = "Paper";
+      return computerChoice
+}
+assignNumber();
+console.log("Computer chooses "+ computerChoice)
+
+//determine win/loss or tie: 
+    //rock wins against scissor
+    //scissor wins against paper
+    //paper wins against rock
+    //same choice is a tie
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === "Rock" && computerChoice === "Paper") {
+        console.log("You lose!")
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You lose!")
+    } else (console.log("It`s a tie...Try again!"));    
+    }   
+determineWinner(playerChoice, computerChoice)
+
+//if successful -> display victory message 
+//else if display defeat message
+//else display a tie message
